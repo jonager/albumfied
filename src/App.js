ba89fdc1b93c83b5e46ddb43b18cc2d9ad1f698e
jonager <jonagerdev@gmail.com>
2018-05-24 19:30:15 -0400

    css modules enabled in project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <h1 >Css module test</h1>
       </div>
     );
   }
