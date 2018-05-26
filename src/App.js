56531b7cdd4a89859ff35897d7f7f3b411851984
jonager <jonagerdev@gmail.com>
2018-05-26 15:34:50 -0400

    env variables file ignored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import styles from './App.css';
-
+import Button from './components/UI/Button/Button';
+import * as utility from './shared/utility';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 >Css module test</h1>
       </div>
     );
   }
