f695d08759dedb72b778f4bddb265c7efac0cda0
jonager <jonagerdev@gmail.com>
2018-05-29 08:33:14 -0400

    redux devtools added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
 
 import './index.css';
 import App from './App';
@@ -13,7 +13,7 @@ let rootReducer = combineReducers({
     auth: authReducer
 });
 
-let store = createStore(rootReducer);
+let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
