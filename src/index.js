24b26043be5854672138d9b4e463ec8a5db0fdea
jonager <jonagerdev@gmail.com>
2018-05-29 08:53:30 -0400

    redux-thunk added to project for async code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
@@ -9,11 +10,15 @@ import registerServiceWorker from './registerServiceWorker';
 
 import authReducer from './store/reducers/auth';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 let rootReducer = combineReducers({
     auth: authReducer
 });
 
-let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
