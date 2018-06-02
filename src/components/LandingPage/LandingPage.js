4a65a2bb3efc1e12232dee3cbb7ed715c536b668
jonager <jonagerdev@gmail.com>
2018-06-02 01:29:23 -0400

    landing page finished

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Button from '../UI/Button/Button';
 import * as utility from '../../shared/utility';
 import Header from '../Layout/Header/Header';
+import styles from './LandingPage.css';
 
 const LandingPage = (props) => {
     return (
         <div>
             <Header isAuth={props.isAuth}/>
-            <Button
-                btnType={'Login'}
-                clicked={utility.spotifyAuth}
-            >Log In</Button>
-            <h1>Hi</h1>
+            <div className={styles.Hero}>
+                <div className={styles.HeroContent}>
+                    <h1>Welcome to AlbumFied!</h1>
+                    <p>An app that lets your organize your Spotify albums by genres/categories.</p>
+                    <a href="https://www.spotify.com/signup/" target="_blank">Sign Up</a>
+                    <Button
+                        btnType={'Login-Hero'}
+                        clicked={utility.spotifyAuth}
+                    >Log In</Button>
+                </div>
+            </div>
         </div>
     );
 }
