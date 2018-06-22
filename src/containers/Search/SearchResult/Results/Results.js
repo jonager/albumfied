b7aeb87e0701193d2c6f42258566cf23cf8a6ad1
jonager <jonagerdev@gmail.com>
2018-06-22 13:16:22 -0400

    card styling done

--- src/containers/Search/SearchResult/Results/Results.js
+++ src/containers/Search/SearchResult/Results/Results.js
@@ -8,7 +8,13 @@ const Results = (props) => {
     let card = props.results.items.map(item => {
             return (
                 <div key={item.id} className={styles.Card}>
-                    {item.images.length !== 0 ? <img style={{width:'200px', height: '200px'}} src={item.images[0].url} alt="Album/Artist"/> : <img src="https://imgplaceholder.com/200x200/75a016/d8db26?text=picture+unavailable&font-size=20" alt="Album/Artist"/>}
+                    {item.images.length !== 0 
+                        ? <img style={{width:'225px', height: '225px',...item.type === 'artist' ? {borderRadius: '50%'} : null}} 
+                            src={item.images[0].url} 
+                            alt="Album/Artist"/> 
+                        : <img style={{...item.type === 'artist' ? {borderRadius: '50%'} : null}} 
+                            src="https://imgplaceholder.com/225x225/75a016/d8db26?text=picture+unavailable&font-size=20" 
+                            alt="Placeholder, not artist picture available"/>}
                     <div className={styles.Info}>
                         <h3>{item.name}</h3> 
                         {item.artists ? <Link to="/" title={item.artists[0].name}>{item.artists[0].name}</Link> : null}
