377a1b3324b7ee5b213c8e0d1ef6691a8456d1c7
jonager <jonagerdev@gmail.com>
2018-08-12 09:33:46 -0400

    firebase apiKey renamed

--- src/fire.js
+++ src/fire.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 import * as env from './env';
 
 var config = {
-    apiKey: env.SPOTIFY_API_KEY,
+    apiKey: env.FIREBASE_API_KEY,
     authDomain: env.AUTH_DOMAIN,
     databaseURL: env.DATABASE_URL,
     projectId: env.PROJECT_ID,
